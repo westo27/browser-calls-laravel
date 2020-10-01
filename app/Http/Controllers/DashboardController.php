ed2798717dd044488d83235772a4a04b089072cd
Tom Weston <tomoweston@gmail.com>
2020-10-01 15:20:53 +0100

    removed ticket

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,16 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Requests;
-use App\Http\Controllers\Controller;
-use App\Ticket;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        $tickets = Ticket::all();
-        return view('supportDashboard', ['tickets' => $tickets]);
+        return view('supportDashboard');
     }
 }
